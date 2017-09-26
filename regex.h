b9b62865ab3a7bf7a02800e915ffc7b771f0b1f1
csssaz <alecassis16@gmail.com>
2017-09-26 01:14:52 +0000

    start parsing regex from input file
    parsing from file seems to be almost ready. add tests and complete hlexer
    
    add line numbers and remove debugs

--- regex.h
+++ regex.h
@@ -1,12 +1,16 @@
 #ifndef REGEX_H
 #define REGEX_H
 
+#include <fstream>
+#include <iostream>
 #include <map>
+#include <set>
 #include <stack>
 #include <string>
 #include <vector>
 
 namespace regex {
+const int BUFF_SIZE = 10000;
 // Node used in NFA directed graph.
 class Node {
  public:
@@ -18,6 +22,8 @@ class Node {
   // letter -> [node_1, node_2, ...]
   std::map<char, std::vector<int>> edges_;
   bool is_accepting_;
+  int token_type_;
+  
 
   Node();
   Node(int index);
@@ -35,18 +41,32 @@ class Node {
 class RegexMatcher {
  public:
   RegexMatcher(std::string infix_regex);
+  // read regex from file
+  RegexMatcher(std::istream& is);
+
+  int NextToken();
+  std::string GetLexeme();
+  int line_no();
 
   bool Matches(const std::string &input);
   std::string postfix_regex();
 
  private:
-  int start_state_, accept_state_;
+  int start_state_;
+  std::set<int> accepting_states_;
   std::vector<Node> states_;
   std::stack<std::tuple<int, int>> build_stack_;
   std::string postfix_regex_;
+  std::string matched_lexeme_;
+  
+  char* forward_;
+  char buffer_[BUFF_SIZE + 3];
+  int line_no_;
+  bool eoi_;
 
   std::tuple<Node, Node> GetStartEndNodes();
 
+  void ConstructPostfix(std::string postfix_regex);
   void DfsEpsilon(int current_state, std::vector<int> &visited);
 
   void AddSymbol(char symbol);
