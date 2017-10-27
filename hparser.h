ec65ef816ae3421dd5634c3b9c60a5cfe77a308d
csssaz <alecassis16@gmail.com>
2017-10-27 13:51:58 +0000

    format interface

--- hparser.h
+++ hparser.h
@@ -129,25 +129,25 @@ class HParser : public Parser {
   std::list<ExprNode*>* expr_list();
 
   ExprNode* expr_or();
-  ExprNode* expr_or_(ExprNode *lhs);
+  ExprNode* expr_or_(ExprNode* lhs);
 
   ExprNode* expr_and();
-  ExprNode* expr_and_(ExprNode *lhs);
+  ExprNode* expr_and_(ExprNode* lhs);
 
   ExprNode* expr_eq();
-  ExprNode* expr_eq_(ExprNode *lhs);
+  ExprNode* expr_eq_(ExprNode* lhs);
 
   ExprNode* expr_rel();
-  ExprNode* expr_rel_(ExprNode *lhs);
+  ExprNode* expr_rel_(ExprNode* lhs);
 
   ExprNode* expr_add();
-  ExprNode* expr_add_(ExprNode *lhs);
+  ExprNode* expr_add_(ExprNode* lhs);
 
   ExprNode* expr_mult();
-  ExprNode* expr_mult_(ExprNode *lhs);
+  ExprNode* expr_mult_(ExprNode* lhs);
 
   ExprNode* expr_unary();
 
   ExprNode* factor();
 };
-#endif //DECAFPARSER_HPARSER_H
+#endif  // DECAFPARSER_HPARSER_H
